aa39c6349a76b82113fa62c2c1214b9b30887c52
ftiasch <ftiasch0@gmail.com>
2012-08-09 09:12:59 +0800

    add new utility

--- geometry/basic-2d/circle-class/page.cpp
+++ geometry/basic-2d/circle-class/page.cpp
@@ -4,4 +4,7 @@ struct Circle {
 
     Circle(const Point &o, double r): o(o), r(r) {}
     Circle(const Point &a, const Point &b): o(0.5 * (a + b)), r(0.5 * (a - b).norm()) {}
+    Circle(const Point &a, const Point &c, const Point &d): o(circumcenter(a, b, c)) {
+        r = (a - o).norm();
+    }
 };
