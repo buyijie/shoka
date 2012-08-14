ef79e6eefe740fc573846ae49b77b7b92e1ca66e
ftiasch <ftiasch0@gmail.com>
2012-08-14 10:36:21 +0800

    use average value instead

--- geometry/basic-2d/circle-class/page.cpp
+++ geometry/basic-2d/circle-class/page.cpp
@@ -5,7 +5,7 @@ struct Circle {
     Circle(const Point &o, double r): o(o), r(r) {}
     Circle(const Point &a, const Point &b): o(0.5 * (a + b)), r(0.5 * (a - b).norm()) {}
     Circle(const Point &a, const Point &c, const Point &d): o(circumcenter(a, b, c)) {
-        r = (a - o).norm();
+        r = ((a - o).norm() + (b - o).norm() + (c - o).norm()) / 3.0;
     }
 
     bool contains(const Point &p) const {
