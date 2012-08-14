727296e30023682c87388c8cc785758ce55805cf
ftiasch <ftiasch0@gmail.com>
2012-08-14 09:11:54 +0800

    fix bug

--- data-structure/string/suffix-array-doubling/page.cpp
+++ data-structure/string/suffix-array-doubling/page.cpp
@@ -20,13 +20,13 @@ void construct(int text[], int n, int array[], int rank[]) {
         }
         memset(count, 0, sizeof(count));
         for (int i = 0; i < n; ++ i) { count[newRank[1][i]] ++; }
-        for (int i = 0; i < n; ++ i) { count[i] += count[i - 1]; }
+        for (int i = 0; i < n; ++ i) { count[i + 1] += count[i]; }
         for (int i = n - 1; i >= 0; -- i) { 
             newArray[-- count[newRank[1][i]]] = i; 
         }
         memset(count, 0, sizeof(count));
         for (int i = 0; i < n; ++ i) { count[newRank[0][i]] ++; }
-        for (int i = 0; i < n; ++ i) { count[i] += count[i - 1]; }
+        for (int i = 0; i < n; ++ i) { count[i + 1] += count[i]; }
         for (int i = n - 1; i >= 0; -- i) {
             array[-- count[newRank[0][newArray[i]]]] = newArray[i];
         }
