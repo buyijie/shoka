e7cd8147ab74d8f108fe80c45f31362f899c8512
ftiasch <ftiasch0@gmail.com>
2012-10-16 21:10:27 +0800

    bug fixed

--- geometry/basic-2d/point-projection/page.cpp
+++ geometry/basic-2d/point-projection/page.cpp
@@ -1,4 +1,4 @@
 Point Point::project(const Point &a, const Point &b) const {
     Point p = *this;
-    return a + dot(p - a, p - b) / (a - b).norm2() * (b - a);
+    return a + dot(p - a, b - a) / (a - b).norm2() * (b - a);
 }
