0a605ed050840d4547520973d34aca20f604344d
ftiasch <ftiasch0@gmail.com>
2013-03-06 13:32:25 +0800

    fix

--- geometry/half-plane-intersection/naive/page.cpp
+++ geometry/half-plane-intersection/naive/page.cpp
@@ -2,8 +2,8 @@ void rebuild(const Point &a, const Point &b) {
     points[n] = points[0];
     int m = 0;
     for (int i = 0; i < n; ++ i) {
-        double s_1 = det(b - a, points[i - 1] - a);
-        double s_2 = det(b - a, points[i] - a);
+        double s_1 = det(b - a, points[i] - a);
+        double s_2 = det(b - a, points[i + 1] - a);
         if (signum(s_1) * signum(s_2) < 0) {
             newPoints[m ++] = (points[i + 1] * s_2 - points[i] * s_1) / (s_2 - s_1);
         }
