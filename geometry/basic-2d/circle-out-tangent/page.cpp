3fb416a31cc21ecb36a456cccd6ac003fd29c68d
ftiasch <ftiasch0@gmail.com>
2013-08-15 13:22:06 +0800

    Update page.cpp

--- geometry/basic-2d/circle-out-tangent/page.cpp
+++ geometry/basic-2d/circle-out-tangent/page.cpp
@@ -4,9 +4,10 @@ pair <Point, Point> tangent(const Circle &a, const Circle &b) {
         return make_pair(a.o + r, b.o + r);
     }
     if (signum(a.r - b.r) > 0) {
-        return tangent(b, a);
+        pair <Point, Point> ret = tangent(b, a);
+        return make_pair(ret.second, ret.first);
     }
     Point p = tangent(a.o, Circle(b.o, b.r - a.r));
     Point r = (p - b.o).normalize() * a.r;
-    return make_pair(a.o + r, b.o + r);
+    return make_pair(a.o + r, p + r);
 }
